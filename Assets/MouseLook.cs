2b2d8aca521be355c0411dfae291e2317171c5f8
Zane Rickert <zane.rickert@gmail.com>
2020-01-08 12:04:01 -0500

    add player movement

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -13,7 +13,7 @@ public class MouseLook : MonoBehaviour
     float xRotation = 0f;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -22,6 +22,8 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity* Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
